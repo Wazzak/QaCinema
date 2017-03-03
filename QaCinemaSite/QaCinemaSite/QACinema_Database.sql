d89a05b7a444d6a9ea4959e0b59e00fa05bc3ab4
Wazzak <p13214528@myemail.dmu.ac.uk>
2017-03-03 14:04:13 +0000

    more things

--- QaCinemaSite/QaCinemaSite/QACinema_Database.sql
+++ QaCinemaSite/QaCinemaSite/QACinema_Database.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[Webpages] (
-    [Page_ID]     INT           NOT NULL,
+    [Page_ID]     INT       IDENTITY (1,1) NOT NULL,
     [Page_Name]   VARCHAR (20)  NULL,
     [Page_Access] CHAR (5)      NULL,
     [URL]         VARCHAR (255) NULL,
@@ -7,13 +7,13 @@
 );
 
 CREATE TABLE [dbo].[Topics] (
-    [Topic_ID]   INT          NOT NULL,
+    [Topic_ID]   INT      IDENTITY (1,1)    NOT NULL,
     [Topic_Name] VARCHAR (10) NULL,
     PRIMARY KEY CLUSTERED ([Topic_ID] ASC)
 );
 
 CREATE TABLE [dbo].[Discussions] (
-    [Discussion_ID] INT          NOT NULL,
+    [Discussion_ID] INT     IDENTITY (1,1)     NOT NULL,
     [Topic_ID]      INT          NULL,
     [Thread_Name]   VARCHAR (20) NULL,
     PRIMARY KEY CLUSTERED ([Discussion_ID] ASC),
@@ -21,7 +21,7 @@ CREATE TABLE [dbo].[Discussions] (
 );
 
 CREATE TABLE [dbo].[Comments] (
-    [Comment_ID]    INT           NOT NULL,
+    [Comment_ID]    INT     IDENTITY (1,1)      NOT NULL,
     [Discussion_ID] INT           NULL,
     [Display_Name]  VARCHAR (15)  NULL,
     [Comment_Text]  VARCHAR (255) NULL,
@@ -30,7 +30,7 @@ CREATE TABLE [dbo].[Comments] (
 );
 
 CREATE TABLE [dbo].[Cinemas] (
-    [Cinema_ID]   INT          NOT NULL,
+    [Cinema_ID]   INT      IDENTITY (1,1)    NOT NULL,
     [Cinema_Name] VARCHAR (15) NULL,
     [Address]     VARCHAR (50) NULL,
     [Postcode]    VARCHAR (8)  NULL,
@@ -38,7 +38,7 @@ CREATE TABLE [dbo].[Cinemas] (
 );
 
 CREATE TABLE [dbo].[Movies] (
-    [Movie_ID]       INT           NOT NULL,
+    [Movie_ID]       INT      IDENTITY (1,1)     NOT NULL,
     [Movie_Name]     VARCHAR (30)  NULL,
     [Classification] VARCHAR (5)   NULL,
     [Release_Date]   DATE          NULL,
@@ -48,13 +48,13 @@ CREATE TABLE [dbo].[Movies] (
 );
 
 CREATE TABLE [dbo].[Actors] (
-    [Actor_ID]   INT          NOT NULL,
+    [Actor_ID]   INT    IDENTITY (1,1)      NOT NULL,
     [Actor_Name] VARCHAR (25) NULL,
     PRIMARY KEY CLUSTERED ([Actor_ID] ASC)
 );
 
 CREATE TABLE [dbo].[Genres] (
-    [Genre_ID] INT          NOT NULL,
+    [Genre_ID] INT     IDENTITY (1,1)     NOT NULL,
     [Type]     VARCHAR (15) NULL,
     PRIMARY KEY CLUSTERED ([Genre_ID] ASC)
 );
@@ -76,7 +76,7 @@ CREATE TABLE [dbo].[Movie_Actors] (
 );
 
 CREATE TABLE [dbo].[Screens] (
-    [Screen_ID]   INT      NOT NULL,
+    [Screen_ID]   INT   IDENTITY (1,1)   NOT NULL,
     [Cinema_ID]   INT      NULL,
     [Screen_No]   INT      NULL,
     [Screen_Type] CHAR (1) NULL,
@@ -86,7 +86,7 @@ CREATE TABLE [dbo].[Screens] (
 );
 
 CREATE TABLE [dbo].[Showings] (
-    [Showing_ID] INT      NOT NULL,
+    [Showing_ID] INT   IDENTITY (1,1)   NOT NULL,
     [Screen_ID]  INT      NULL,
     [Movie_ID]   INT      NULL,
     [Date]       DATE     NULL,
@@ -97,7 +97,7 @@ CREATE TABLE [dbo].[Showings] (
 );
 
 CREATE TABLE [dbo].[Bookings] (
-    [Booking_ID]   INT          NOT NULL,
+    [Booking_ID]   INT     IDENTITY (1,1)     NOT NULL,
     [Showing_ID]   INT          NULL,
     [Seat_No]      INT          NULL,
     [Buyers_Email] VARCHAR (30) NULL,
@@ -115,7 +115,7 @@ CREATE TABLE [dbo].[Tickets] (
 );
 
 CREATE TABLE [dbo].[Movie_Reviews] (
-    [Review_ID]    INT           NOT NULL,
+    [Review_ID]    INT      IDENTITY (1,1)     NOT NULL,
     [Movie_ID]     INT           NULL,
     [Review_Text]  VARCHAR (255) NULL,
     [Display_Name] VARCHAR (15)  NULL,
